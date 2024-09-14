2be1a62d676d3a528c8c854b60e712114600b5bb
Janosch <97047112+joschi3957@users.noreply.github.com>
2024-09-14 22:53:10 +0200

    main.py

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ from discord.ext.commands import has_permissions
 
 from images import *
 
-TOKEN = 'token'
+TOKEN = 'enter youre token here'
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True
-client = commands.Bot(command_prefix='f!', intents=intents, description='j4n0sch_gaming', help_command=None)
+client = commands.Bot(command_prefix='!', intents=intents, description='j4n0sch_gaming', help_command=None)
 
 
 @client.event
 async def on_ready():
-    activity_type = discord.ActivityType.listening
-    activity_name = 'J4n0sch_gaming'
+    activity_type = discord.ActivityType.streaming
+    activity_name = '!help'
     await client.change_presence(activity=discord.Activity(type=activity_type, name=activity_name))
 
     print('I am ready for the use')
     print(' ')
 
 
-# On message function
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.event
 async def on_message(message):
     await client.process_commands(message)
@@ -31,29 +30,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if user_message.lower() == 'monke':
-        await message.channel.send('https://tenor.com/view/overwatch-echo-winston-echo-echo-ow-meme-gif<W-17200162')
+    if user_message.lower() == 'tokyo ghoul':
+        await message.channel.send('https://i.pinimg.com/originals/07/25/65/07256543b7243633bd70f1b22237b8ba.gif')
 
     if user_message.lower() == 'godo':
-        await message.channel.send(f'{message.author.mention} sta godendo', file=discord.File(fes_godo),
+        await message.channel.send(f'{message.author.mention} sta godendo', await message.channel.send('https://i.pinimg.com/originals/6a/a7/d4/6aa7d437075cf9ee449452f300f2894f.gif'),
                                    reference=message)
     if user_message.lower() == 'not funny':
-        await message.channel.send(file=discord.File(mei_not_funny), reference=message)
+        await message.channel.send('https://i.pinimg.com/originals/36/4e/a6/364ea6cd7bfd00a0260aaf6e2602cd4d.gif'),
 
-    if user_message.lower() == 'milord gay':
-        await message.channel.send('_yessir_')
+    if user_message.lower() == 'Am i gay ?':
+        await message.channel.send('https://i.pinimg.com/originals/da/19/eb/da19ebf0fe2336c362f7c23b20dd8661.gif')
 
 
-# Test command
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 async def test(ctx):
     await ctx.send(ctx.message.content)
     print(ctx.message.content)
 
 
-# Show his or the tagged member's pfp
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 async def pfp(ctx):
     try:
@@ -62,36 +59,33 @@ async def pfp(ctx):
         user = ctx.author
     if not user:
         return
-    pfp = user.avatar_url
+    pfp = user.avatar.url
     embed = discord.Embed(title=f'{user}profile pictures', description=user, color=0xe91e63)
     embed.set_image(url=(pfp))
     await ctx.channel.send(embed=embed)
 
 
-# Show a list of all commands
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 async def commands(ctx):
-    commands = '**f!pfp** [member tag]\n Show the pfp of the member tagged\n\n' \
-               '**f!test** \n Command used for testing the bot\n\n' \
+    commands = '**!pfp** [member tag]\n Show the pfp of the member tagged\n\n' \
+               '**!test** \n Command used for testing the bot\n\n' \
                '_coming soon_'
     embed = discord.Embed(title='You can find the commands here', description=commands, color=110011)
     await ctx.channel.send(embed=embed)
 
 
-# Help command
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 async def help(ctx):
-    help = '**Prefix:** f!\n' \
-           '**Commands:** f!commands\n' \
+    help = '**Prefix:** !\n' \
+           '**Commands:** !commands\n' \
            '_coming soon_'
     embed = discord.Embed(title='Hi bruh, how can i help you?', description=help, color=110011)
     await ctx.channel.send(embed=embed)
 
 
-# Kick/Ban commands
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 @has_permissions(kick_members=True)
 async def kick(ctx, memeber: discord.Member, *, reason=None):
@@ -118,8 +112,7 @@ async def ban_error(ctx, error):
         await ctx.send("You don't have permission to ban people")
 
 
-# Punching people
-# ----------------------------------------------------------------------------------------------------------------------
+
 @client.command()
 async def punch(ctx):
     try:
@@ -129,9 +122,8 @@ async def punch(ctx):
         await ctx.send('Are you punching yourself?')
     if not user:
         return
-    await ctx.channel.send(f'<@{author}> punched <@{user}>', file=discord.File(markiplier_punch))
+    await ctx.channel.send(f'<@{author}> punched <@{user}>', await ctx.channel.send('https://i.pinimg.com/originals/48/d5/59/48d55975d1c4ec1aa74f4646962bb815.gif'))
 
 
-# ----------------------------------------------------------------------------------------------------------------------
 
 client.run(TOKEN)
